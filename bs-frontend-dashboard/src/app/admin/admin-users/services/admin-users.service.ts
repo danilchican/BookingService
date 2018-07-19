8cc2a67db087ca1983ded202f81ada090e2f1206
Artur Kulik <artur.kulik.97@mail.ru>
2018-07-19 22:40:28 +0300

    BS-12 Command work testing. Use server API.

--- bs-frontend-dashboard/src/app/admin/admin-users/services/admin-users.service.ts
+++ bs-frontend-dashboard/src/app/admin/admin-users/services/admin-users.service.ts
@@ -7,7 +7,13 @@ import {UserDto} from "../../../core/users/dto/user-dto";
 @Injectable()
 export class AdminUsersService {
 
-  private static readonly LOAD_USERS = 'http://localhost:8080/users';
+  private static readonly LOAD_USERS = 'http://localhost:8080/clients';
+
+  private static readonly CREATE_USER = 'http://localhost:8080/clients';
+
+  private static readonly EDIT_USER = 'http://localhost:8080/clients/';
+
+  private static readonly DELETE_USER = 'http://localhost:8080/clients/';
 
   constructor(
     private http: HttpClient
@@ -20,14 +26,17 @@ export class AdminUsersService {
   }
 
   public createUser(user: User): Observable<User[]> {
-    return this.loadUsers();
+    return this.http.post(AdminUsersService.CREATE_USER, user)
+      .switchMap(() => this.loadUsers());
   }
 
   public editUser(user: User): Observable<User[]> {
-    return this.loadUsers();
+    return this.http.put(`${AdminUsersService.EDIT_USER}${user.id}`, user)
+      .switchMap(() => this.loadUsers());
   }
 
   public deleteUser(user: User): Observable<User[]> {
-    return this.loadUsers();
+    return this.http.delete(`${AdminUsersService.DELETE_USER}${user.id}`)
+      .switchMap(() => this.loadUsers());
   }
 }
